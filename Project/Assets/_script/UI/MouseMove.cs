ba8def14da6e678138d647e6fd324e9fc4d1c1c5
fenglin <374992436@qq.com>
2017-05-30 21:57:43 +0800

    ui功能实现
    Signed-off-by: fenglin <374992436@qq.com>

---
+++ Project/Assets/_script/UI/MouseMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    public MouseOffset[] MouseOffsets;
+    private readonly Vector3 _zero = new Vector3(1920 / 2f, 1080 / 2f, 0);
+    void Update()
+    {
+        var offset = Input.mousePosition - _zero;
+        MoveOffset(offset.x, offset.y);
+        print(offset);
+    }
+
+    private void MoveOffset(float x, float y)
+    {
+        if (MouseOffsets == null) return;
+        foreach (var mouseOffset in MouseOffsets)
+        {
+            mouseOffset.Offset(x, y);
+        }
+    }
+}
